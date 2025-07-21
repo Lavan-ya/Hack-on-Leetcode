e47d339d21544ad00899e1a8e51d6ef62702db0f
Lavanya Selvaraaj <126905371+Lavan-ya@users.noreply.github.com>
2025-07-21 19:03:54 +0200

    Time: 0 ms (100%), Space: 17.9 MB (27.33%) - LeetHub

--- 2427-first-letter-to-appear-twice/2427-first-letter-to-appear-twice.py
+++ 2427-first-letter-to-appear-twice/2427-first-letter-to-appear-twice.py
@@ -1,11 +1,9 @@
 class Solution:
     def repeatedCharacter(self, s: str) -> str:
-        dict={}
-        split_String = list(s)
-        for i in range(len(split_String)):
-            if split_String[i] in dict:
-                dict[split_String[i]]=dict[split_String[i]]+1
-                if dict[split_String[i]] >1:
-                    return split_String[i]            
+        sets=set()
+        for i in s:
+            if i in sets:
+                return i
             else:
-                dict[split_String[i]]=1
+                sets.add(i)
+        
