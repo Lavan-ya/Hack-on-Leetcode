fb2946c89744caec5e74a1ea37c81bc5b8081379
Lavanya Selvaraaj <126905371+Lavan-ya@users.noreply.github.com>
2025-08-05 11:11:02 +0200

    Time: 1 ms (48.07%), Space: 17.8 MB (84.55%) - LeetHub

--- 0360-sort-transformed-array/0360-sort-transformed-array.py
+++ 0360-sort-transformed-array/0360-sort-transformed-array.py
@@ -1,9 +1,30 @@
 class Solution:
     def sortTransformedArray(self, nums: List[int], a: int, b: int, c: int) -> List[int]:
-        lst=[]
-        for num in nums:
-            func= a*(num*num)+b*num+c
-            lst.append(func)
-        return sorted(lst)
-
+        res=[0]*len(nums)
+        if a>=0:
+            idx=len(nums)-1
+        else:
+            idx=0
         
+        l,r=0,len(nums)-1
+        while l<=r:
+            fl=a*(nums[l]*nums[l]) + b*nums[l] + c
+            fr=a*(nums[r]*nums[r]) + b*nums[r] + c
+
+            if a>=0 and fl>=fr:
+                res[idx]=fl
+                idx-=1
+                l+=1
+            elif a>=0 and fr>fl:
+                res[idx]=fr
+                idx-=1
+                r-=1
+            elif a<0 and fl<=fr:
+                res[idx]=fl
+                idx+=1
+                l+=1
+            else:
+                res[idx]=fr
+                idx+=1
+                r-=1
+        return res
